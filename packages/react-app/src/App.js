a7791c99c357b355000f55bdf7608a8bb91045ad
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-09-30 17:18:40 +0900

    import

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,7 +1,11 @@
-import React from "react"
+import React from "react";
+import { useEthers } from "@usedapp/core";
+
+import styles from "./styles";
+import { uniswapLogo } from "./assets";
 
 const App = () => {
-  return <p>Hello, Web3!</p>
-}
+  return <p>Hello, Web3!</p>;
+};
 
 export default App;
