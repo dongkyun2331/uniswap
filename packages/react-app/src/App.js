fe6b6817e030ae03908b3baa4315e921942b76bb
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-10-02 00:02:16 +0900

    title

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -14,7 +14,17 @@ const App = () => {
             alt="uniswap logo"
             className="w-16 h-16 object-contain"
           />
+          WalletButton
         </header>
+
+        <div className={styles.exchangeContainer}>
+          <h1 className={styles.headTitle}>Uniswap 2.0</h1>
+          <p className={styles.subTitle}>Exchange tokens in seconds</p>
+
+          <div className={styles.exchangeBoxWrapper}>
+            <div className={styles.exchangeBox}></div>
+          </div>
+        </div>
       </div>
     </div>
   );
