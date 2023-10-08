89422de6e0e32c97dd1c3a6aa52cbfc9f9190c48
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-10-09 00:04:03 +0900

    amount balance

--- packages/react-app/src/components/Exchange.js
+++ packages/react-app/src/components/Exchange.js
@@ -11,10 +11,31 @@ import {
 import { ethers } from "ethers";
 import { parseUnits } from "ethers/lib/utils";
 
+import {
+  getAvailableTokens,
+  getCounterpartTokens,
+  findPoolByTokens,
+  isOperationPending,
+  getFailureMessage,
+  getSuccessMessage,
+} from "../";
 import { ROUTER_ADDRESS } from "../config";
+import { AmountIn, AmountOut, Balance } from "./";
+import styles from "../styles";
 
 const Exchange = (pools) => {
-  return <div>Exchange</div>;
+  return (
+    <div className="flex flex-col w-full items-center">
+      <div className="mb-8">
+        <AmountIn />
+        <Balance />
+      </div>
+      <div className="mb-8 w-[100%]">
+        <AmountOut />
+        <Balance />
+      </div>
+    </div>
+  );
 };
 
 export default Exchange;
