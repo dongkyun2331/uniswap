4c1984e949b8b41b8c23a02f35bd15d3fd3e7bbe
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-10-08 15:59:55 +0900

    address

--- packages/react-app/src/components/WalletButton.js
+++ packages/react-app/src/components/WalletButton.js
@@ -4,10 +4,19 @@ import { shortenAddress, useEthers, useLookupAddress } from "@usedapp/core";
 import styles from "../styles";
 
 const WalletButton = () => {
-  const [rendered, setRendered] = useState("");
+  const [accountAddress, setAccountAddress] = useState("");
   const { ens } = useLookupAddress();
   const { account, activateBrowserWallet, deactivate } = useEthers();
 
+  useEffect(() => {
+    if (ens) {
+      setAccountAddress(ens);
+    } else if (account) {
+      setAccountAddress(shortenAddress(account));
+    } else {
+      setAccountAddress("");
+    }
+  }, [account, ens, setAccountAddress]);
   return (
     <button
       onClick={() => {
@@ -19,8 +28,7 @@ const WalletButton = () => {
       }}
       className={styles.walletButton}
     >
-      {rendered === "" && "Connect Wallet"}
-      {rendered !== "" && rendered}
+      {accountAddress || "Connect Wallet"}
     </button>
   );
 };
