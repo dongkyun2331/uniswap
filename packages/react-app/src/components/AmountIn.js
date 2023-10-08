224af38404738eceb9e9f55dfd6f70eb6f6b2860
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-10-09 00:20:47 +0900

    AmountIn input 0.0

--- packages/react-app/src/components/AmountIn.js
+++ packages/react-app/src/components/AmountIn.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+
+import { chevronDown } from "../assets";
+import styles from "../styles";
+import { useOnClickOutside } from "../utils";
 
 const AmountIn = () => {
-  return <div>AmountIn</div>;
+  return (
+    <div className={styles.amountContainer}>
+      <input
+        placeholder="0.0"
+        type="number"
+        value=""
+        disabled={false}
+        onChange={() => {}}
+        className={styles.amountInput}
+      />
+    </div>
+  );
 };
 
 export default AmountIn;
